68b0399452d5f8d53fc611657563a4055f832260
cunho <ancunho@naver.com>
2024-06-05 23:13:23 +0900

    QuerydslPredicateExecutor 및 querydsl추가
    build.gradle에 implementation "com.querydsl:querydsl-jpa:${dependencyManagement.importedProperties['querydsl.version']}:jakarta"
        implementation "com.querydsl:querydsl-core"
        implementation "com.querydsl:querydsl-collections"
        annotationProcessor "com.querydsl:querydsl-apt:${dependencyManagement.importedProperties['querydsl.version']}:jakarta" // querydsl JPAAnnotationProcessor 사용 지정 이거 꼭해줘야뎀.
    
    원인: springboot 3.X버전 이후로는 스타일이 이럼.
    springboot 2.X버전은 또 다름.

--- board/src/main/java/org/fastcampus/board/repository/ArticleCommentRepository.java
+++ board/src/main/java/org/fastcampus/board/repository/ArticleCommentRepository.java
@@ -2,8 +2,12 @@ package org.fastcampus.board.repository;
 
 import org.fastcampus.board.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>
+{
 }
--- board/src/main/java/org/fastcampus/board/repository/ArticleRepository.java
+++ board/src/main/java/org/fastcampus/board/repository/ArticleRepository.java
@@ -2,8 +2,10 @@ package org.fastcampus.board.repository;
 
 import org.fastcampus.board.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article> {
 }
